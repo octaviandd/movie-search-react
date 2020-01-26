8a7aeebb4aae2d448e7a4588e92068463c8594b9
Octavian David <45077244+octaviandd@users.noreply.github.com>
2020-01-26 18:14:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,40 @@
 
 import React from "react";
 import "./App.css";
+import fetchMovie from "./components/api";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeMovie: "Avengers",
+      loading: true,
+      allMovies: []
+    };
+  }
+
+  componentDidMount() {
+    this.getData(this.state.activeMovie);
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.activeMovie !== this.state.activeMovie) {
+      this.getData(this.state.activeMovie);
+    }
+  }
+
+  getData(movie) {
+    this.setState({
+      loading: true
+    });
+
+    this.setState({
+      allMovies: fetchMovie(movie),
+      loading: false
+    });
+  }
+
   render() {
     return <div></div>;
   }
