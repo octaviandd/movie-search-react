c092d27759bad4d9f9419034d809bd618e7bca3d
Octavian David <45077244+octaviandd@users.noreply.github.com>
2020-01-27 21:05:42 +0000

    Add files via upload

---
+++ src/components/loading.js
@@ -0,0 +1,33 @@
+/** @format */
+
+import React from "react";
+
+class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "Loading"
+    };
+  }
+
+  componentDidMount() {
+    var stopper = this.state.text + "...";
+
+    this.interval = window.setInterval(() => {
+      this.state.text === stopper
+        ? this.setState({ text: "Loading" })
+        : this.setState(currentState => ({ text: currentState.text + "." }));
+    }, 300);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+
+  render() {
+    return <p>{this.state.text}</p>;
+  }
+}
+
+export default Loading;
