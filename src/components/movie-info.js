8aa0c6f88749c52df2e4756dd913829d0de9fe04
Octavian David <45077244+octaviandd@users.noreply.github.com>
2020-01-30 20:42:43 +0000

    Update movie-info.js

--- src/components/movie-info.js
+++ src/components/movie-info.js
@@ -5,15 +5,40 @@ import React from "react";
 class MovieList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      allSimilarMovies: this.props.movies,
+      activeMovie: this.props.movies[0],
+      count: 1
+    };
   }
+
+  increaseCount = () => {
+    this.setState(currentState => {
+      return {
+        count: currentState.count + 1
+      };
+    });
+    console.log(this.state.count);
+  };
+
+  handleNextMovie = () => {
+    this.setState(currentState => {
+      return {
+        activeMovie: this.props.movies[this.state.count]
+      };
+    });
+    this.increaseCount();
+  };
 
   render() {
     return (
       <div id="main-movie">
         <div id="main-movie-2">
-          <h1>{this.props.movies.original_title}</h1>
+          <h1>{this.props.movie.original_title}</h1>
           <img
-            src={`http://image.tmdb.org/t/p/w342/${this.props.movies.poster_path}`}
+            src={`http://image.tmdb.org/t/p/w342/${this.props.movie.poster_path}`}
+            alt="movie poster"
           ></img>
           <table>
             <thead>
@@ -25,14 +50,33 @@ class MovieList extends React.Component {
             </thead>
             <tbody>
               <tr>
-                <td>{Math.floor(this.props.movies.popularity)} / 100</td>
-                <td>{this.props.movies.vote_count}</td>
-                <td>{this.props.movies.vote_average}</td>
+                <td>{Math.floor(this.props.movie.popularity)} %</td>
+                <td>{this.props.movie.vote_count}</td>
+                <td>{this.props.movie.vote_average}</td>
               </tr>
             </tbody>
           </table>
           <h3>Overview:</h3>
-          <h4>{this.props.movies.overview}</h4>
+          <h4>{this.props.movie.overview}</h4>
+        </div>
+
+        <div id="suggested-movies">
+          <h2>Similar Movies</h2>
+          <div id="suggested-movies-2">
+            <div>
+              <i
+                onClick={() => this.handleNextMovie()}
+                className="far fa-arrow-alt-circle-left"
+              ></i>
+            </div>
+            <img
+              src={`http://image.tmdb.org/t/p/w185/${this.state.activeMovie.poster_path}`}
+              alt="suggested"
+            ></img>
+            <div>
+              <i className="far fa-arrow-alt-circle-right"></i>{" "}
+            </div>
+          </div>
         </div>
       </div>
     );
